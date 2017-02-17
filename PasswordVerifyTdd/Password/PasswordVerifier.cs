7af2255908ff0257618924f50646a4a8f28aaf90
PC1701\biondo <igormirko.biondo@gmail.com>
2017-02-17 16:20:05 +0100

    add special chars check

--- PasswordVerifyTdd/Password/PasswordVerifier.cs
+++ PasswordVerifyTdd/Password/PasswordVerifier.cs
@@ -9,6 +9,7 @@ namespace Password
     {
         List<string> alphabet = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         List<string> numbers = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        List<string> specialChars = new List<string> { "\\", "|", "!", "£", "$", "%", "/", "(", ")", "=", "?", "'", "\"", "^", "@", "#", "[", "]", "<", ">", ",", ":", ".", ";", "-", "_", "*", "+" };
 
         public bool Verify(string password)
         {
@@ -21,7 +22,10 @@ namespace Password
             var containsNumber = (from n in numbers
                                   where password.Contains(n)
                                   select n).Any();
-            return (containsNumber && containsUpperLower && password.Length >= 8);
+            var containsSpecialChars = (from s in specialChars
+                                        where password.Contains(s)
+                                        select s).Any();
+            return (containsSpecialChars && containsNumber && containsUpperLower && password.Length >= 8);
         }
     }
 }
--- PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
+++ PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
@@ -19,7 +19,7 @@ namespace TestPassword
         /// Test the lenght (at least 8)
         /// </summary>
         /// <param name="password"></param>
-        [TestCase("123456aA")]
+        [TestCase("12345@aA")]
         public void TestLenght(string password)
         {
             var result = pass.Verify(password);
@@ -27,24 +27,27 @@ namespace TestPassword
             Assert.GreaterOrEqual(password.Length, 8);
         }
 
-
         /// <summary>
         /// Test string has at least one upper letter, 
         /// one tolower letter, one number
         /// and not is null or empty
+        /// ------------------------
+        /// features: should be contains a special char
+        /// NO SPACE
         /// </summary>
         /// <param name="password"></param>
         /// <param name="expected"></param>
         [TestCase("", false)]
         [TestCase(null, false)]
-        [TestCase("123Aa", false)]
+        [TestCase("123Aa!", false)]
+        [TestCase("12Ab!$", false)]
         [TestCase("12345678", false)]
-        [TestCase("12345678", false)]
-        [TestCase("123456aA", true)]
+        [TestCase("123456aA", false)]
         [TestCase("1234567A", false)]
-        [TestCase("123456aA", true)]
+        [TestCase("123456aA", false)]
         [TestCase("AaBbCcDd", false)]
-        [TestCase("Aa12Cc34", true)]
+        [TestCase("Aa12Cc34", false)]
+        [TestCase("Aa12Cc!@", true)]
         public void TestPassword(string password, bool expected)
         {
             var result = pass.Verify(password);
