aa20bcb2be1658591595094a81fdf0a12a4d042c
PC1701\biondo <igormirko.biondo@gmail.com>
2017-02-17 15:28:42 +0100

    test lenght greater 8 (red)

---
+++ PasswordVerifyTdd/Password/Password.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Password
+{
+    class Password
+    {
+        internal object Verify(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
+++ PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
@@ -10,6 +10,17 @@ namespace TestPassword
     [TestFixture]
     public class TestPasswordVerify
     {
+        private Password pass;
+        [SetUp]
+        public void SetUp(){
+            pass= new Password();
+        }
 
+        [TestCase("12345678")]
+        public void TestLenght(string password){
+            var result = pass.Verify(password);
+            Assert.IsTrue(result);
+            Assert.GreaterOrEqual(password.Length,8);
+        }
     }
 }
