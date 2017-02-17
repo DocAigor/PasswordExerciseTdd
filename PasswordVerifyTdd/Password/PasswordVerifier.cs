4cc0d95d4a027606cf39aabd8b6b30220cda2c31
PC1701\biondo <igormirko.biondo@gmail.com>
2017-02-17 15:31:26 +0100

    fix class Password

--- PasswordVerifyTdd/Password/PasswordVerifier.cs
+++ PasswordVerifyTdd/Password/PasswordVerifier.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace TestPassword
+namespace Password
 {
-    public class Password
+    public class PasswordVerifier
     {
         public bool Verify(string password)
         {
