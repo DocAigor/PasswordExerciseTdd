e16e576a2fc8f1b8e7420e3aaecec5c82e620340
PC1701\biondo <igormirko.biondo@gmail.com>
2017-02-17 15:54:47 +0100

    test string contains a to upper character

--- PasswordVerifyTdd/Password/PasswordVerifier.cs
+++ PasswordVerifyTdd/Password/PasswordVerifier.cs
@@ -7,9 +7,15 @@ namespace Password
 {
     public class PasswordVerifier
     {
+        List<string> dictionary = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         public bool Verify(string password)
         {
-            return (password.Length >= 8);
+            if (string.IsNullOrEmpty(password)) return false;
+
+            var containsUpper = (from c in dictionary
+                                where password.Contains(c.ToUpper())
+                                select c).Any();      
+            return (containsUpper && password.Length >= 8);
         }
     }
 }
--- PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
+++ PasswordVerifyTdd/TestPassword/TestPasswordVerify.cs
@@ -17,11 +17,36 @@ namespace TestPassword
             pass = new PasswordVerifier();
         }
 
-        [TestCase("12345678")]
+        /// <summary>
+        /// Test the lenght (at least 8)
+        /// </summary>
+        /// <param name="password"></param>
+        [TestCase("1234567A")]
         public void TestLenght(string password){
             var result = pass.Verify(password);
             Assert.IsTrue(result);
             Assert.GreaterOrEqual(password.Length,8);
+        }
+
+        /// <summary>
+        /// Test string null or empty
+        /// </summary>
+        /// <param name="password"></param>
+        [TestCase("")]
+        [TestCase(null)]
+        public void TestStringNull(string password)
+        {
+            var result = pass.Verify(password);
+            Assert.IsFalse(result);
+        }
+
+        [TestCase("12345678",false)]
+        [TestCase("12345678", false)]
+        [TestCase("1234567A", true)]
+        public void TestMustContainUpper(string password,bool expected)
+        {
+            var result = pass.Verify(password);
+            Assert.AreEqual(result,expected);
         }
     }
 }
